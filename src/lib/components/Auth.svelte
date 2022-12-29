65557250a471d4a2d96bf7faf250cdeb96144180
Jeremie Thönnißen <33234850+jere2101@users.noreply.github.com>
2022-12-29 12:19:02 +0100

    update

--- src/lib/components/Auth.svelte
+++ src/lib/components/Auth.svelte
@@ -3,10 +3,25 @@
 	let loading = false;
 	let email: string;
 
+	const getURL = () => {
+		let url =
+			import.meta.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+			import.meta.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+			'http://localhost:3000/';
+		// Make sure to include `https://` when not localhost.
+		url = url.includes('http') ? url : `https://${url}`;
+		// Make sure to including trailing `/`.
+		url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+		return url;
+	};
+
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { error } = await supabase.auth.signInWithOtp({ email });
+			const { error } = await supabase.auth.signInWithOtp({
+				email,
+				options: { emailRedirectTo: getURL() }
+			});
 
 			if (error) throw error;
 
